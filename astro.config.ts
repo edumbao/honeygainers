8be5191b83e3ff2ab4a543adb60854e50241d61f
edumbao <philnewsxyz@gmail.com>
2026-06-23 11:11:23 +0800

    update config

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import { transformerFileName } from "./src/utils/transformers/fileName";
 import config from "./astro-paper.config";
 
 export default defineConfig({
-  site: config.site.url,
+  site: "https://edumbao.github.io/",
   base: "/honeygainers/", // <--- ADD THIS LINE
   integrations: [
     mdx(),
